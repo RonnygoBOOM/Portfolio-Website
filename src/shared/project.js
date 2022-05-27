a0d618a175f94d6f1273dfafeb339bd6a054d459
Scheibelr <scheibelr@gmail.com>
2022-05-26 21:52:08 -0400

    added website boolean to object file

--- src/shared/project.js
+++ src/shared/project.js
@@ -7,6 +7,7 @@ export const PROJECT = [
         link: "https://youtu.be/eM5Gqr_QZQk",
         hoverText: "Click to watch a short video demonstrating the functionality of this website",
         alt: "Video to show functionality of my cnft website project",
+        website: true,
     },
     {
         id: 2,
@@ -15,6 +16,7 @@ export const PROJECT = [
         coverImg: "images/dnd.png",
         link: "https://youtu.be/yg5eJtQf_0c",
         alt: "Video to show functionality of my D&D website project",
+        website: true,
     },
     {
         id: 3,
@@ -23,6 +25,7 @@ export const PROJECT = [
         coverImg: "images/tenzies.png",
         link: "https://youtu.be/Tvqb91-Q7eA",
         alt: "Video to show functionality of my tenzies game project",
+        website: true,
     },
     {
         id: 4,
@@ -31,6 +34,7 @@ export const PROJECT = [
         coverImg: "images/drunken-dragon.png",
         link: "https://dndnftgame.com/",
         alt: "Video to show functionality of my meme generator website project",
+        website: true,
     },
     {
         id: 5,
@@ -39,6 +43,7 @@ export const PROJECT = [
         coverImg: "images/meme-generator.png",
         link: "https://youtu.be/38K8db9JjmA",
         alt: "Video to show functionality of my meme generator website project",
+        website: false, /*keep as video */
     },
     {
         id: 6,
@@ -47,6 +52,7 @@ export const PROJECT = [
         coverImg: "images/react-native-mobile-app.png",
         link: "https://youtu.be/A-d6ugHbLbk",
         alt: "Video to show functionality of my Nucampsite mobile app project",
+        website: false,
     },
     {
         id: 7,
@@ -55,6 +61,7 @@ export const PROJECT = [
         coverImg: "images/airbnb-clone.png",
         link: "https://drive.google.com/file/d/1_P1E0ZWthQsYKmlDokDLXKVOQCpehDnl/view?usp=sharing",
         alt: "Image to show the display of my Airbnb landing page project",
+        website: false,
     },
     {
         id: 8,
@@ -63,5 +70,6 @@ export const PROJECT = [
         coverImg: "images/digital-business-card.png",
         link: "https://drive.google.com/file/d/1dO7T4Kq1NJ1lRPXCvDgjYc0nDgV0CoqK/view?usp=sharing",
         alt: "Image to show the display of my digital business card project",
+        website: true,
     },
 ]
