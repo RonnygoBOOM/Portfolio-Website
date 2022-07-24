c36712786bd447b2c251090c1f44a826bc9dfbbc
Scheibelr <scheibelr@gmail.com>
2022-07-23 22:00:02 -0400

    contact form added and hooked up to emailjs

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,38 @@
-import React from "react";
-import { Card, CardTitle, CardText, Container } from "reactstrap";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import {
+  FormGroup,
+  Input,
+  Label,
+  Card,
+  CardTitle,
+  CardText,
+  Container,
+} from "reactstrap";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_255as5l",
+        "template_gnaxgrc",
+        form.current,
+        "UP6M60oa6x6YcjGcY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <>
       <div className="surround-div">
@@ -11,17 +42,46 @@ function Contact() {
               <CardTitle className="contact-title">Contact Me</CardTitle>
             </a>
             <CardText className="contact-text">
-              I live in Orlando, FL, and am looking for local or remote
-              career opportunities. I am also willing to relocate to Huntsville, AL.
+              I live in Orlando, FL, and am looking for local or remote career
+              opportunities. I am also willing to relocate to Huntsville, AL.
             </CardText>
             <CardText className="contact-text">
               I look forward to hearing from you!
             </CardText>
             <a href="mailto:scheibelr@gmail.com">
-              <CardText className="contact-text">
-              scheibelr@gmail.com
-              </CardText>
+              <CardText className="contact-text">scheibelr@gmail.com</CardText>
             </a>
+            <form className="contact-form" ref={form} onSubmit={sendEmail}>
+              <FormGroup>
+                <Label>Name or Company</Label>
+                <Input
+                  type="text"
+                  name="name_company"
+                  id="name_company"
+                  placeholder="Name/Company"
+                ></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label>Email</Label>
+                <Input
+                  type="text"
+                  name="return_email"
+                  placeholder="Email Address"
+                  id="return_email"
+                ></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label>Message</Label>
+                <Input
+                  type="textarea"
+                  name="message_text"
+                  id="message_text"
+                ></Input>
+                <div className="send-button-div">
+                  <input className="send-button" type="submit" value="Send" />
+                </div>
+              </FormGroup>
+            </form>
           </Card>
         </Container>
       </div>
@@ -30,3 +90,34 @@ function Contact() {
 }
 
 export default Contact;
+
+/*import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+*/
